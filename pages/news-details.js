aa301d052cf6e6cdc638cbe5d4ecdeb8364d32a3
Linework <75095654+Noeldamien@users.noreply.github.com>
2021-11-22 17:49:31 +0100

    Update news-details.js

--- pages/news-details.js
+++ pages/news-details.js
@@ -11,6 +11,50 @@ export default function NewsDetails() {
               <h2>LWC Pay Fees Feature is Coming Soon</h2>
               <p>
                 Crypo is the DEX platform powered by Linework Corporation !
+    
+    Cryptocurrencies are expected to gradually become a way of life but the technology is still 
+    in its infancy: there are many adoption barriers to break and its needs adoption by as many 
+    people as possible.
+    
+    With almost everyone wanting to have a share in the now $126 billion crypto market, there is 
+    need to find out which coin and company to go for, which depends on the versatility and ability
+    of its base technology, uniqueness of business idea and growth potential.
+
+     Faced with the event of new crypto payment technologies in the world with “bitcoin” as an 
+    initial reference, Linework is positioning itself as a major blockchain player in the world 
+    of virtualization of global processes and of globalization in terms of communication decentralized. 
+    
+     Already through its social network, Linework is a global player in the decentralization of the 
+    assets of everyone by dematerializing web marketing structures, physical contacts wherever you 
+    are in the world.
+    
+    Linework is a central player in change and in area 4.0, which brings together crypto- payments 
+    which in particular can be used on Linework platforms as a benchmark.
+
+    LWC (Linework Coin) is therefore positioned as a choice, strategic and secure value for your 
+  investments in cryptocurrencies. LWC operates on the blockchain and benefits from its own block, 
+    its security based on the HA512 algorithm and sequential block encryption in the Ethereum Blockchain-based.
+
+    LWC does not require a super IT structure and is an accessible token on the CRYPO EXCHANGE platform. 
+    
+    Finally, the integrity of LWC is guaranteed in the same way as Bitcoin, in a decentralized manner 
+    accessible by a personal WALLET and secured by the "seed phrase", a "strong" password and an 
+    interface accessible by an application available for IOS, MacOS, Android or Windows. The LWC 
+    is transferable to a nano ledger in the form of a secure USB key.
+
+    It is more reassuring to invest in rising stocks that have not yet reached their peak state, which 
+  therefore do not risk falling. The BTC falls more and more every day and the holders of assets in BTC, 
+    other than having the faculty to transform them into physical currency, can no longer make investments 
+    on this currency since it no longer brings anything in the trading rooms.
+
+    The LWC is therefore positioned as a real investment tool, for funding the Linework Social Network 
+    and for enhancing artisanal, commercial, social and societal activity in developing countries or 
+    countries in economic difficulty or in deficit of democracy.
+
+    Linework is performing as a real geopolitical and geostrategic tool working for the liberty and 
+    freedoms in the world, but not also, Linework Social Network involved a new financial and social 
+    power in parallel of central banking organization for completing the actual financing institution 
+    to be stronger than ever.
               </p>
               <p>
                 As the development of a Token is paired with an exchange platform
